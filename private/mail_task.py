c870450e746d744548785e6cbf36bfb568d1e0d3
Amber Doctor <amberdoctor@gmail.com>
2012-05-13 12:36:13 -0600

    Assignment 3 Submission 1

---
+++ private/mail_task.py
@@ -0,0 +1,19 @@
+import time
+while True:
+    ## set the query up to find emails that haven't been send and that have a send date that has past
+    query = (db.email.sent_on==None)&(db.email.email_send_date < request.now)
+    ## grab the next email to send
+    next_email = db(query).select().first()
+    ## check if there is an email to send
+    if next_email:
+        ## send the email
+        ret = mail.send(to=next_email.email_to,
+                   subject=next_email.email_subject,
+                   message=next_email.email_message)
+        ## update the database
+        next_email.update_record(sent_on=request.now,
+                                    failed = not ret)
+        db.commit()
+    ## else sleep
+    else:
+        time.sleep(60) #that's 60 second sleep
